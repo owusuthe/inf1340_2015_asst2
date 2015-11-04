e6b085f397889d37fef5f518c8fcbf6209ecfd76
owusuthe <therese.owusu@mail.utoronto.ca>
2015-11-04 08:03:29 -0500

    Update to exercise 1, still need to append removed consonants

--- exercise1.py
+++ exercise1.py
@@ -14,13 +14,52 @@ __license__ = "MIT License"
 
 def pig_latinify(word):
     """
-    Describe your function
+    #For a given word in english, returns the word in pig latin
 
-    :param :
-    :return:
-    :raises:
+    :param : word
+    :return: new_word + 'ay' or new_word + 'yay'
+    :raises: except TypeError
 
     """
-    result = ""
+word = ""
+#vowels = ('a', 'e', 'i', 'o', 'u')
+#consonants = ('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z')
+vowels = ("aeiou")
+consonants = ("bcdfghjklmnpqrstvwxyz")
+"""
+def pig_latinify(word):
+    append_yay = "yay"
+    append_ay = "ay"
+    if word[0] in vowels:
+        return word + append_yay
+    elif word[0] in consonants:
+        return word.lstrip("bcdfghjklmnpqrstvwxyz" ) + append_ay
+    else:
+        print "Enter a new word"
 
-    return result
+print pig_latinify("branch")
+
+
+    #result = ""
+
+    #return result
+"""
+
+word = ""
+#vowels = ('a', 'e', 'i', 'o', 'u')
+#consonants = ('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z')
+vowels = ("aeiou")
+consonants = ("bcdfghjklmnpqrstvwxyz")
+
+def pig_latinify(word):
+    append_yay = "yay"
+    append_ay = "ay"
+    consonants_removed = word.lstrip(consonants)
+    if word[0] in vowels:
+        return word + append_yay
+    elif word[0] in consonants:
+        return consonants_removed + append_ay
+    else:
+        print "Enter a new word"
+        
+print pig_latinify("scratch")
